5d8e6030d129c2e0adea86de274b2bdc23777605
Marcelo Gadotti <eng.marcelogadotti@gmail.com>
2023-03-01 09:46:10 -0300

    up to use url from img
    Signed-off-by: Marcelo Gadotti <eng.marcelogadotti@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,34 +14,51 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 import photos from "./photos";
 
-const slides = photos.map(({ src, width, height, images }) => ({
-    src,
-    width,
-    height,
-    srcSet: images.map((image) => ({
-        src: image.src,
-        width: image.width,
-        height: image.height,
-    })),
-}));
-
 const App = () => {
-    const [index, setIndex] = useState(-1);
+  const [index, setIndex] = useState(-1);
 
-    return (
-        <>
-            <PhotoAlbum photos={photos} layout="rows" targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+  return (
+    <>
+      <PhotoAlbum
+        photos={photos}
+        layout="rows"
+        targetRowHeight={200}
+        onClick={({ index }) => setIndex(index)}
+        columns={(containerWidth) => {
+          if (containerWidth < 400) return 1;
+          if (containerWidth < 600) return 2;
+          return 3;
+        }}
+        renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto }) => (
+          <div style={{ position: "relative", ...wrapperStyle }}>
+            {renderDefaultPhoto({ wrapped: true })}
+            {photo.id && (
+              <div
+                style={{
+                  position: "absolute",
+                  overflow: "hidden",
+                  backgroundColor: "rgba(255 255 255 / .5)",
+                  inset: "auto 0 0 0",
+                  padding: 8,
+                }}
+              >
+                {photo.id}
+              </div>
+            )}
+          </div>
+        )}
+      />
 
-            <Lightbox
-                slides={slides}
-                open={index >= 0}
-                index={index}
-                close={() => setIndex(-1)}
-                // enable optional lightbox plugins
-                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
-            />
-        </>
-    );
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
+  );
 };
 
 export default App;
--- src/photos.ts
+++ src/photos.ts
@@ -1,48 +1,20 @@
-const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
-
-const unsplashLink = (id: string, width: number, height: number) =>
-    `https://source.unsplash.com/${id}/${width}x${height}`;
+const breakpoints = [1, 2, 3];
 
 const unsplashPhotos = [
-    { id: "Osq7UAVxIOI", width: 1080, height: 780 },
-    { id: "Dhmn6ete6g8", width: 1080, height: 1620 },
-    { id: "RkBTPqPEGDo", width: 1080, height: 720 },
-    { id: "Yizrl9N_eDA", width: 1080, height: 721 },
-    { id: "KG3TyFi0iTU", width: 1080, height: 1620 },
-    { id: "Jztmx9yqjBw", width: 1080, height: 607 },
-    { id: "-heLWtuAN3c", width: 1080, height: 608 },
-    { id: "xOigCUcFdA8", width: 1080, height: 720 },
-    { id: "1azAjl8FTnU", width: 1080, height: 1549 },
-    { id: "ALrCdq-ui_Q", width: 1080, height: 720 },
-    { id: "twukN12EN7c", width: 1080, height: 694 },
-    { id: "9UjEyzA6pP4", width: 1080, height: 1620 },
-    { id: "sEXGgun3ZiE", width: 1080, height: 720 },
-    { id: "S-cdwrx-YuQ", width: 1080, height: 1440 },
-    { id: "q-motCAvPBM", width: 1080, height: 1620 },
-    { id: "Xn4L310ztMU", width: 1080, height: 810 },
-    { id: "iMchCC-3_fE", width: 1080, height: 610 },
-    { id: "X48pUOPKf7A", width: 1080, height: 160 },
-    { id: "GbLS6YVXj0U", width: 1080, height: 810 },
-    { id: "9CRd1J1rEOM", width: 1080, height: 720 },
-    { id: "xKhtkhc9HbQ", width: 1080, height: 1440 },
+    { id: 1, src: "http://192.168.0.190:8000/marcelo/imagens/1.jpeg" },
+    { id: 2, src: "http://192.168.0.190:8000/marcelo/imagens/2.jpeg" },
+    { id: 3, src: "http://192.168.0.190:8000/marcelo/imagens/3.jpeg" },
 ];
 
 const photos = unsplashPhotos.map((photo) => {
-    const width = breakpoints[0];
-    const height = (photo.height / photo.width) * width;
+    const width = 1024;
+    const height = 768;
 
     return {
-        src: unsplashLink(photo.id, width, height),
+        src: photo.src,
         width,
         height,
-        images: breakpoints.map((breakpoint) => {
-            const height = Math.round((photo.height / photo.width) * breakpoint);
-            return {
-                src: unsplashLink(photo.id, breakpoint, height),
-                width: breakpoint,
-                height,
-            };
-        }),
+        id: photo.id
     };
 });
 
